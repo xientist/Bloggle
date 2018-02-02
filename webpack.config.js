b318ebc880eb315cb170d8395310d9d5b36da697
unknown <t_huynh927@yahoo.com>
2018-02-02 13:09:09 -0800

    heroku

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpack = require('webpack');
 
-process.env.NODE_ENV = 'development' || process.env.NODE_ENV;
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {
     require('dotenv').config({ path: '.env.test' });
