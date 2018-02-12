e3a204fd911f6a9d0783ca65c079c2d2856f4360
unknown <t_huynh927@yahoo.com>
2018-02-12 03:15:06 -0800

    made styling edits for front page bloggles, about 50% done

--- public/src/components/BlogListItem.js
+++ public/src/components/BlogListItem.js
@@ -13,11 +13,19 @@ class BlogListItem extends React.Component {
         return (
             <div  className="list-item">
                 <div>
-                    <h3 className="list-item__title">{this.props.title}</h3>
-                    <Link to={`/profile/${this.props.uid}`}><div className="portrait" >{this.props.userImage !== '' ? <img className="list-image" src={this.props.userImage} /> : <img className="list-image" src="http://www.theeaglefan.com/ELB/Anonymous.jpg" />}</div></Link>
-                    <Link to={`/profile/${this.props.uid}`}><p className="list-item__sub-title">By {this.props.username}</p></Link>
-                    <span className="list-item__sub-title">{moment(this.props.createdAt).format( 'MMMM Do, YYYY h:mm:ss a')}</span>
-                    <h3 className="list-item__data">{this.props.blog}</h3>
+                    <div className="bloggle-user-info">
+                        <div>
+                            <Link to={`/profile/${this.props.uid}`}><div className="portrait" >{this.props.userImage !== '' ? <img className="list-image" src={this.props.userImage} /> : <img className="list-image" src="http://www.theeaglefan.com/ELB/Anonymous.jpg" />}</div></Link>
+                        </div>
+                        <div>
+                            <h3 className="list-item__title">{this.props.title}</h3>
+                            <Link to={`/profile/${this.props.uid}`} className="no-decoration"><p className="list-item__sub-title">By {this.props.username}</p></Link>
+                            <p className="list-item__date">{moment(this.props.createdAt).format( 'MMMM Do, YYYY h:mm:ss a')}</p>
+                        </div>
+                    </div>
+                    <div className="bloggle-comment">
+                        <h3 className="list-item__data">{this.props.blog}</h3>
+                    </div>
                     <ListItemBar id={this.props.id} uid={this.props.uid} isAuthenticated={this.props.isAuthenticated} likes={this.props.likes} userLikes={this.props.userLikes}/>
                 </div>
             </div>
